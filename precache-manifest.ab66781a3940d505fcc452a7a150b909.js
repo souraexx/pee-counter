118cf317949517486addbc10306409043e9cbe87
Souradeep Dutta <sourae.47@gmail.com>
2020-05-11 04:04:23 +0530

    Updates

---
+++ precache-manifest.ab66781a3940d505fcc452a7a150b909.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7da977bf0ab57ea70a24366412378229",
+    "url": "/pee-counter/index.html"
+  },
+  {
+    "revision": "b0c984a629a7d555653f",
+    "url": "/pee-counter/static/css/main.16e19ad3.chunk.css"
+  },
+  {
+    "revision": "352ba1650273c3bf3699",
+    "url": "/pee-counter/static/js/2.68ae4a30.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pee-counter/static/js/2.68ae4a30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0c984a629a7d555653f",
+    "url": "/pee-counter/static/js/main.6079f588.chunk.js"
+  },
+  {
+    "revision": "12f268101934ac3a28b1",
+    "url": "/pee-counter/static/js/runtime-main.47ae93fc.js"
+  },
+  {
+    "revision": "1455f6b50e5ca422d01e3c51c7088791",
+    "url": "/pee-counter/static/media/sweat.1455f6b5.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.79254e2e43c2eac9c3b1ce95c6d28bd3.js"
+  "/pee-counter/precache-manifest.ab66781a3940d505fcc452a7a150b909.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pee-counter/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
